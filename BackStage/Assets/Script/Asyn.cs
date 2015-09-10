56c7961e6dafca646698057402303405b4322b04
Natthaphat <sir.icedteenz@gmail.com>
2015-09-10 13:40:49 +0700

    ASyn_Test

---
+++ BackStage/Assets/Script/Asyn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asyn : MonoBehaviour {
+
+    void Start()
+    {
+        print("Starting " + Time.time);
+        
+        StartCoroutine(WaitAndChange(2.0F));
+        print("Before WaitAndChange Finishes " + Time.time);
+    }
+    IEnumerator WaitAndChange(float waitTime)
+    {
+      
+        //GetComponent<Light>().color = Color.Lerp(Color.red, Color.magenta, waitTime * Time.deltaTime);
+        yield return new WaitForSeconds(waitTime);
+        GetComponent<Light>().color = Color.white;
+
+
+    }
+}
